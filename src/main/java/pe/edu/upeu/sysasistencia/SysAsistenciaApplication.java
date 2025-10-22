2d4a3dffd573f373edfb17176bd9a4500803cd80
Patricia-Apaza <patriciaapaza80@gmail.com>
2025-10-22 03:13:18 -0500

    Prueba

--- src/main/java/pe/edu/upeu/sysasistencia/SysAsistenciaApplication.java
+++ src/main/java/pe/edu/upeu/sysasistencia/SysAsistenciaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SysAsistenciaApplication {
-    //Probando Integración Continúa con Jenkins
     public static void main(String[] args) {
         SpringApplication.run(SysAsistenciaApplication.class, args);
     }
